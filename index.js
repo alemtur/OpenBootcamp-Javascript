8231a7d4afddf0dcc928de3739570e59e04cc24c
alemtur <63555475+alemtur@users.noreply.github.com>
2022-10-21 15:51:01 -0300

    Cursos 15 - Lección 1875
     Cursos 15 - Lección 1875

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+const alejandro = {
+    nombre: "Alejandro",
+    edad: 41,
+    isDesarrollador: true,
+    nacimiento: new Date("february 9 1981"),
+    "libro-favorito": {
+        titulo: "El principito",
+        autor: "Exupery",
+        fecha: "01/01/70",
+        url: "www.url.com"
+    }
+}
+
+console.log(alejandro.nombre, alejandro.edad, alejandro.isDesarrollador,
+    alejandro.nacimiento, alejandro["libro-favorito"].titulo, alejandro["libro-favorito"].autor, 
+    alejandro["libro-favorito"].fecha, alejandro["libro-favorito"].url);
