34288a8c6217b3d96a84fb27c772cbab2ccea85c
alemtur <63555475+alemtur@users.noreply.github.com>
2022-11-02 09:55:39 -0300

    ejercicio-leccion-1878
    ejercicio-leccion-1878

---
+++ ejercicio-leccion-1878.js
@@ -0,0 +1,15 @@
+let altura_cmts = 178;
+console.log(typeof altura_cmts);
+
+let altura_metros = 1.78;
+
+let peso = 80.14;
+
+let altura_redondeada = Math.ceil(altura_metros);
+console.log(altura_redondeada);
+
+let peso_redondeado = Math.floor(peso);
+console.log(peso_redondeado);
+
+let maximo = (Number.MAX_VALUE + 1 == Number.MAX_VALUE);
+console.log(maximo);
