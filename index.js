06a46c3b459793d2dd862f36f969033b04eb8527
alemtur <63555475+alemtur@users.noreply.github.com>
2022-11-27 09:27:03 -0300

    OpenBootcamp-cursos15-leccion1890
    OpenBootcamp-cursos15-leccion1890

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-class Estudiante {
-    nombre = "Alejandro";
-    asignaturas = ["Javascript", "HTML", "CSS"];
-    
-    obtenDatos(){
-        const objeto = {
-            nombre: this.nombre,
-            asignaturas: this.asignaturas
-        }
-        return objeto;
-    }
-}
+// Sesión 4 - Strings (cadena de caracteres)
+let str_dbl = "Hola soy un texto con comillas dobles";
+let str_sng = "Hola soy un texto con comillas simples";
 
-const nuevoEstudiante = new Estudiante();
-console.log(nuevoEstudiante.obtenDatos());
+
+console.log(str_dbl);
+console.log(str_sng);
+
+let str_comillas = "El otro día me dijo literalmente \"ve a sacar la basura\"";
+let str_comillas_simples = "El otro día me dijo literalmente \"ve a sacar la basura\"";
+let str_comillas_dobles = "El otro día me dijo literalmente 've a sacar la basura'";
+let str_comillas_simples_2 = "El otro día me dijo literalmente 've a sacar la basura'";
+
+console.log(str_comillas);
+console.log(str_comillas_simples);
+console.log(str_comillas_dobles);
+console.log(str_comillas_simples_2);
+
+// Comillas invertidas (backticks) 
+// Permiten introducir variables dentro de nuestro texto
+// Permiten los saltos de linea dentro del string
+let str_backticks = "Hola esto es un string con backticks";
+console.log(str_backticks);
+
+let nombre = "Gorka";
+let saludo = `Hola, ${nombre} bienvendido`;
+
+console.log(saludo);
+
+// Plantillas HTML
+let plantilla = `
+<html>
+    <h1>Página web de ${nombre}</h1>
+    <p>Este es un párrafo</p>
+
+</html>
+`;
+console.log(plantilla);
+
+
 
