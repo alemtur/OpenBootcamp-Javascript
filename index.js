a3294f1d3f649722eeb1886700c802ece9f4aa67
alemtur <63555475+alemtur@users.noreply.github.com>
2022-12-03 08:39:14 -0300

    OpenBootcamp-cursos15-leccion1891
    OpenBootcamp-cursos15-leccion1891

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-// Sesión 4 - Strings (cadena de caracteres)
-let str_dbl = "Hola soy un texto con comillas dobles";
-let str_sng = "Hola soy un texto con comillas simples";
+const boton1 = document.getElementById("btn1")
 
+boton1.addEventListener("click", () => {
+    alert("Hola, NO estoy utilizando jQuery")
+})
 
-console.log(str_dbl);
-console.log(str_sng);
+const boton2 = document.querySelector("#btn2")
 
-let str_comillas = "El otro día me dijo literalmente \"ve a sacar la basura\"";
-let str_comillas_simples = "El otro día me dijo literalmente \"ve a sacar la basura\"";
-let str_comillas_dobles = "El otro día me dijo literalmente 've a sacar la basura'";
-let str_comillas_simples_2 = "El otro día me dijo literalmente 've a sacar la basura'";
-
-console.log(str_comillas);
-console.log(str_comillas_simples);
-console.log(str_comillas_dobles);
-console.log(str_comillas_simples_2);
-
-// Comillas invertidas (backticks) 
-// Permiten introducir variables dentro de nuestro texto
-// Permiten los saltos de linea dentro del string
-let str_backticks = "Hola esto es un string con backticks";
-console.log(str_backticks);
-
-let nombre = "Gorka";
-let saludo = `Hola, ${nombre} bienvendido`;
-
-console.log(saludo);
-
-// Plantillas HTML
-let plantilla = `
-<html>
-    <h1>Página web de ${nombre}</h1>
-    <p>Este es un párrafo</p>
-
-</html>
-`;
-console.log(plantilla);
-
-
-
+boton2.addEventListener("click", () => {
+    alert("Hola, estoy utilizando jQuery")
+})
